c0139227496b8f0e6d2c73f1e23d15298e754131
Sourav <133347351+sp-project-23@users.noreply.github.com>
2023-05-14 14:21:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
 	<head>
-	<title>Sourav Pramanick's Resume Registry</title>
+	<title>S P's Resume Registry</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 	</head>
 	<body>
 	<div class="container col-6">
-	<div class="col" align="center"><h2>Sourav Pramanick's Resume Registry</h2></div>
+	<div class="col" align="center"><h2>S P's Resume Registry</h2></div>
 	<?php
 	require_once "pdo.php";
 	session_start();
